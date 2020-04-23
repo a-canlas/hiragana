3b8576ff8c9acea5c944fae17c2173e73d4a89d5
matchashiba <recr38@gmail.com>
2020-04-23 20:07:20 +0000

    Able to display hiragana A Able to display hiragana E '

--- src/display.jsx
+++ src/display.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-function Display(props) {
-  return (
-    props.displayVal.length === 0 ? <h2>Type something in...</h2> : <h2>{props.displayVal}</h2>
-  );
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.convert = this.convert.bind(this);
+  }
+
+  convert() {
+    let abc = this.props.displayVal;
+    abc = abc.toLowerCase().split('');
+    let converted = '';
+    for (let i = 0; i < abc.length; i++) {
+      if (abc[i] === 'a') {
+        converted += '\u3042';
+      } else if (abc[i] === 'e') {
+        converted += '\u3048';
+      }
+    }
+    return converted;
+  }
+
+  render() {
+    return (
+      this.props.displayVal.length === 0 ? <h2>Type something in...</h2> : <h2>{this.convert()}</h2>
+    );
+  }
 }
 
 export default Display;
