6489230bbe219b5c06a08da5dab5b8a1439d51ef
matchashiba <recr38@gmail.com>
2020-04-24 19:38:45 +0000

    Changed conditional in 'i' from 'n' to '\u3093'

--- src/display.jsx
+++ src/display.jsx
@@ -116,7 +116,7 @@ class Display extends React.Component {
         } else if (converted[len - 1] === 'p') {
           converted = converted.slice(0, len - 1);
           converted += '\u3074';
-        } else if (converted[len - 1] === 'n') {
+        } else if (converted[len - 1] === '\u3093') {
           converted = converted.slice(0, len - 1);
           converted += '\u306B';
         } else if (converted[len - 1] === 'r') {
